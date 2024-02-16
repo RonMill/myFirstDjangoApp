32869639919b47c40908fae8782c943a1571a2e2
Ronny <romi1001@stud.hs-kl.de>
2024-02-16 14:43:36 +0100

    added customized user model + added avatars

--- base/views.py
+++ base/views.py
@@ -3,11 +3,11 @@ from django.db.models import Q
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic, Message
-from .forms import RoomForm, UserForm
+# from django.contrib.auth.forms import UserCreationForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationFrom
 
 
 # rooms = [
@@ -23,15 +23,15 @@ def loginPage(request):
     return redirect('home')
 
   if request.method == 'POST':
-    username = request.POST.get('username').lower()
+    email = request.POST.get('email')
     password = request.POST.get('password')
       
     try:
-      user = User.objects.get(username=username)
+      user = User.objects.get(email=email)
     except:
       messages.error(request, "This user does not exist")
 
-    user = authenticate(request, username=username, password=password)
+    user = authenticate(request, email=email, password=password)
 
     if user is not None:
       login(request, user)
@@ -50,10 +50,10 @@ def logoutUser(request):
 
 #! Register
 def registerPage(request):
-  form = UserCreationForm()
+  form = MyUserCreationFrom()
 
   if request.method == 'POST':
-    form = UserCreationForm(request.POST)
+    form = MyUserCreationFrom(request.POST)
     if form.is_valid():
       user = form.save(commit=False)
       user.username = user.username.lower()
@@ -195,7 +195,7 @@ def updateUser(request):
   form = UserForm(instance=user)
 
   if request.method == 'POST':
-    form = UserForm(request.POST, instance=user)
+    form = UserForm(request.POST, request.FILES ,instance=user)
     if form.is_valid():
       user = form.save(commit=False)
       user.username = user.username.lower()
--- myFirstDjangoApp/settings.py
+++ myFirstDjangoApp/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "corsheaders",
 ]
 
+AUTH_USER_MODEL = 'base.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -122,11 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/assets/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+MEDIA_ROOT = BASE_DIR / 'static/assets'
 # STATIC_ROOT =
 
 # Default primary key field type
